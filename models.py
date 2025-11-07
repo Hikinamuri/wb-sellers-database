d79a881c3ab0bddeb2dc9d1c68bc2f352e44280c
Hikinamuri <hikinamuri@mail.ru>
2025-11-07 21:35:30 +0300

    updates models

--- models.py
+++ models.py
@@ -1,22 +1,25 @@
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, JSON, Enum
+from sqlalchemy import (
+    Column, Integer, String, Float, Text, DateTime, ForeignKey,
+    Enum, BigInteger
+)
+from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
 import enum
 import sys, os
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))  # добавляем корень проекта
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from database.db import Base
-from sqlalchemy import Column, Integer, String, Float, Text, ForeignKey
-from sqlalchemy.orm import relationship
-from database.db import get_session
+
 
 class ProductStatus(str, enum.Enum):
     processing = "В обработке"
     pending = "Ожидает выкладки"
     posted = "Выложен"
     canceled = "Отменен"
-
+    
 class User(Base):
     __tablename__ = "users"
+
     id = Column(Integer, primary_key=True, index=True)
     tg_id = Column(String, unique=True)
     name = Column(String)
@@ -24,17 +27,19 @@ class User(Base):
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     products = relationship("Product", back_populates="user")
 
+
 class Product(Base):
     __tablename__ = "products"
+
     id = Column(Integer, primary_key=True)
     user_id = Column(String, ForeignKey("users.tg_id"))
     url = Column(String)
     name = Column(String)
-    description = Column(Text)  # можно хранить длинный текст
+    description = Column(Text)
     image_url = Column(String)
 
-    # Новые поля
-    wb_id = Column(Integer, nullable=True)            # артикула/ID WB
+    # 🔹 Совместимые с таблицей типы
+    wb_id = Column(BigInteger, nullable=True)
     brand = Column(String, nullable=True)
     seller = Column(String, nullable=True)
     rating = Column(Float, nullable=True)
@@ -42,13 +47,14 @@ class Product(Base):
     basic_price = Column(Float, nullable=True)
     discount = Column(Integer, nullable=True)
     stocks = Column(Integer, nullable=True)
-    stocks_by_size = Column(JSON, nullable=True)      # json array
-    images = Column(JSON, nullable=True)              # json array (список url)
+    stocks_by_size = Column(JSONB, nullable=True)
+    images = Column(JSONB, nullable=True)
+    category = Column(String(50), nullable=True)
 
-    info = Column(JSON, nullable=True)                # доп. JSON
+    info = Column(JSONB, nullable=True)
     status = Column(Enum(ProductStatus), default=ProductStatus.processing)
-    scheduled_date = Column(DateTime)
+    scheduled_date = Column(DateTime, nullable=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
-    price = Column(Float)
+    price = Column(Float, nullable=True)
 
     user = relationship("User", back_populates="products")
